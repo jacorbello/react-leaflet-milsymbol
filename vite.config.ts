fb054e8fb73aa9c8ab4b44eac2257137ec77dd39
Jeremy Corbello <jacorbello@gmail.com>
2025-03-11 13:11:20 -0500

    Add terser for minification and update package configurations

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,6 @@ export default defineConfig({
       fileName: (format) => `react-leaflet-milsymbol.${format}.js`,
     },
     rollupOptions: {
-      // Make sure to externalize deps that shouldn't be bundled
       external: [
         ...Object.keys(pkg.devDependencies || {}),
         'react/jsx-runtime',
@@ -28,28 +27,23 @@ export default defineConfig({
         'react-leaflet',
       ],
       output: {
-        // Provide global variables to use in the UMD build
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
           'react-leaflet': 'ReactLeaflet',
           'leaflet': 'L',
           'milsymbol': 'milsymbol',
+          'react/jsx-runtime': 'jsxRuntime',
         },
-        // Add banner comment at the top of generated files
         banner: '/*\n * react-leaflet-milsymbol\n * A React Leaflet v4 wrapper for milsymbol\n */',
       },
     },
-    // Generate sourcemaps for easier debugging
     sourcemap: true,
-    // Minify the output
     minify: 'terser',
   },
-  // Avoid Vite's default HTML handling to focus on library output
   optimizeDeps: {
     exclude: ['milsymbol'],
   },
-  // Add resolve aliases for cleaner imports in your code
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
